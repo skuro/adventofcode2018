838ef5b44153503b980adb5192cd2bc3bb4bea67
Carlo Sciolla <carlo.sciolla@gmail.com>
2018-12-01 17:17:51 +0100

    Refactoring
    - no need to check if an infinite seq terminates
    - `if` is better than `cond` when you only have 2 branches
    - some docs

--- src/advent2018/day1.clj
+++ src/advent2018/day1.clj
@@ -1,32 +1,30 @@
 (ns advent2018.day1
+  "Solve https://adventofcode.com/2018/day/1"
   (:require [clojure.java.io :as io]))
 
+;;;;;;;;;;;;; yak shaving ;;;;;;;;;;;;;
 (def read-input (comp io/reader io/resource))
 
 (defn parse-input [input-file]
   (map read-string (line-seq input-file)))
 
+;;;;;;;;;;;;; part one ;;;;;;;;;;;;;
 (defn apply-freq-diffs [diffs]
   (reduce + 0 diffs))
-
-(defn find-first-dupe [diffs]
-  (loop [[freq & freqs] (reductions + 0 (cycle diffs))
-         seen?  #{}]
-    (cond
-      (seen? freq)
-      freq
-
-      (seq freqs)
-      (recur freqs
-             (conj seen? freq))
-
-      :else
-      (println "nope"))))
 
 (defn solve-part-one [file-path]
   (-> (read-input file-path)
       parse-input
       apply-freq-diffs))
+
+;;;;;;;;;;;;; part two ;;;;;;;;;;;;;
+(defn find-first-dupe [diffs]
+  (loop [[freq & freqs] (reductions + 0 (cycle diffs))
+         seen?  #{}]
+    (if (seen? freq)
+      freq
+      (recur freqs
+             (conj seen? freq)))))
 
 (defn solve-part-two [file-path]
   (-> (read-input file-path)
